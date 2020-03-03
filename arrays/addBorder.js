9938323055318b926a8b2a7a44ee15f436663785
Miguel Peralta <eudysperalta723@gmail.com>
2020-03-03 09:59:28 -0800

    add borders problem in python

---
+++ arrays/addBorder.js
@@ -0,0 +1,29 @@
+// PYTHON CODE
+
+
+
+// def addBorder(picture):
+//     # create bopy of picture
+//     # get get the size
+//     #create borders
+//     framed = list(picture)
+//     size = len(picture[0]) + 2
+//     borders = "*" * size
+    
+//     # for each item in picture if length is less than size
+//     # add start to edges
+//     for i in range(len(picture)):
+//         print(picture[i])
+//         if(len(framed[i]) < size):
+//             framed[i] = framed[i] + "*"
+//             framed[i] = "*" + framed[i]
+
+//     # add top and bottom borders
+//     framed.insert(0, borders)
+//     framed.append(borders)
+
+//     print(framed)
+//     return framed
+
+
+   
