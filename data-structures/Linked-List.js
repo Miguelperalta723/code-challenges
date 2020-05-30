ea82f16a2b17071d6b9f7b0b2e21cc67e9549dea
Miguel Peralta <34365332+Miguelperalta723@users.noreply.github.com>
2020-05-29 21:25:00 -0700

    Update Linked-List.js

--- data-structures/Linked-List.js
+++ data-structures/Linked-List.js
@@ -1,1 +1,77 @@
-i observed an unknown person breaking into an emplo
+class Node {
+    constructor(data, next = null){
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor(){
+        this.head = null;
+    }
+
+    insertFirst(data){
+        this.head = new Node(data, this.head);
+    }
+
+    size(){
+        let counter = 0;
+        let node = this.head;
+
+        while(node){
+            counter++;
+            node = node.next;
+        }
+
+        return counter;
+    }
+
+    deleteFirst(){
+      this.head = this.head.next
+    }
+
+    getLast(){
+      let node = this.head
+
+      while(node){
+        if(node.next){
+          node = node.next
+        }else {
+          return node
+        }
+      }
+    }
+
+    clear(){
+      this.head = null
+    }
+
+    removeLast(){
+      let prev = this.head
+      let node = prev.next
+
+      while(node){
+        if(node.next){
+          node = node.next
+        }else {
+          node = null
+        }
+      }
+    }
+}
+
+const list = new LinkedList;
+list.insertFirst(5)
+list.insertFirst(10)
+list.insertFirst(50000)
+list.insertFirst(1)
+// list.deleteFirst()
+// list.deleteFirst()  
+// list.clear()
+list.removeLast()
+
+
+console.log(list)
+// console.log(list.head.next)
+console.log(list.size())
+console.log(list.getLast())
