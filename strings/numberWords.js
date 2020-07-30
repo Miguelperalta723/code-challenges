6eda42d7005ea1cc231053573ee121a0c50e75bf
Miguel Peralta <34365332+Miguelperalta723@users.noreply.github.com>
2020-07-30 08:02:48 -0700

    Update numberWords.js

--- strings/numberWords.js
+++ strings/numberWords.js
@@ -6,3 +6,12 @@ function countWords(str) {
 	}
 	return count
 }
+
+// function countWords(str) {
+// 	let arr = str.split(" ")
+// 	let count = 0;
+// 	for(word in arr){
+// 		count++
+// 	}
+// 	return count
+// }
