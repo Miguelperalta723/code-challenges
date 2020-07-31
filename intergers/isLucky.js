b8d644735006d34793186f668cfdf71b9569e94a
Miguel Peralta <34365332+Miguelperalta723@users.noreply.github.com>
2020-07-31 12:40:31 -0700

    Update isLucky.js

--- intergers/isLucky.js
+++ intergers/isLucky.js
@@ -33,3 +33,29 @@ function isLucky(n) {
 // isLucky(n) = true;
 // For n = 239017, the output should be
 // isLucky(n) = false.
+
+  
+// function isLucky(n) {
+//     let num =   n.toString().split("")
+  
+//     let point = num.length/2
+
+//     let first = num.slice(0,point)
+//     let second = num.slice(point)
+
+//     let firstSum = 0
+//     let secondSum = 0
+//     console.log(first)
+//     console.log(second)
+//     for(let i = 0; i < first.length; i++){
+//         firstSum += parseInt(first[i])
+//         secondSum += parseInt(second[i]) 
+//     }
+    
+//     if(firstSum === secondSum){
+//        return true
+//     } else {
+//         return false
+//     }
+    
+// }
