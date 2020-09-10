79b62e8c65b36570f188b34289a672c5a8278146
Miguel Peralta <34365332+Miguelperalta723@users.noreply.github.com>
2020-09-09 19:14:15 -0700

    Update twonumsum.py

--- AXX/twonumsum.py
+++ AXX/twonumsum.py
@@ -1,1 +1,18 @@
 
+
+def twoNumberSum(array, targetSum):
+    # Write your code here.
+    
+	map = {}
+	//for each num in array
+  //if the targetsum minus current num 
+  //you get what opp that adds to targetsum
+  // if that is in hash map
+  // then you found it return the pair
+  //else add it to the map
+	for num in array:
+		if targetSum - num in map:
+			return [targetSum - num, num]
+		else:
+			map[num] = True;
+	return []
